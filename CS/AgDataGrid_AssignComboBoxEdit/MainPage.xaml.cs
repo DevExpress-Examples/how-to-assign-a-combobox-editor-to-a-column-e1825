a6b28aa4d9cd5e64673787a5e28be4ded28b699c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_AssignComboBoxEdit/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using DevExpress.AgDataGrid;
+using DevExpress.AgEditors.Settings;
+
+namespace AgDataGrid_AssignComboBoxEdit {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+            InitComboBoxColumn(grid.Columns["Country"]);
+        }
+        private void InitComboBoxColumn(AgDataGridColumn column) {
+            ((ComboBoxEditSettings)column.EditSettings).ItemsSource = GetCountries((List<Product>)grid.DataSource);
+        }
+        private List<string> GetCountries(List<Product> ds) {
+            List<string> list = new List<string>();
+            foreach (Product p in ds) {
+                if (!list.Contains(p.Country))
+                    list.Add(p.Country);
+            }
+            return list;
+        }
+    }
+}
